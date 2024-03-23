cf4d0aba7a6ec1adb45d567687cce9205945326b
Juan Diego <juandiegosamp@hotmail.com>
2024-03-23 18:22:53 -0500

    Agregada funcionalidad de limpieza de habitaciones en clase Housekeeping

--- src/main/java/com/mycompany/lab2hotelf/Housekeeping.java
+++ src/main/java/com/mycompany/lab2hotelf/Housekeeping.java
@@ -1,14 +1,20 @@
 package com.mycompany.lab2hotelf;
 
-public class Housekeeping extends Employee{
+import java.util.ArrayList;
 
+public class Housekeeping extends Employee{
+    
     public Housekeeping(String location, String position, String shift, String name, String id) {
         super(location, position, shift, name, id);
     }
 
-    public void cleanRoom(){
-        //guarda el registro de aseo
+    public void cleanRoom(String roomNo, Hotel hotel){
+        ArrayList<Room> roomList = hotel.getRoomsList();
+            for(int i = 0; i < roomList.size(); i++){
+                if (roomList.get(i).getRoomNo().equals(roomNo)){
+                    roomList.get(i).setClean(true);
+                    break;
+                }
+            }
     }
-
-   
 }
